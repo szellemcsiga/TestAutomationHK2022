b7cea4c8b05b68eff2633b20529881f67100ddd5
szellemcsiga <horvathklaudia86@gmail.com>
2022-06-16 20:52:47 +0200

    second commit

--- src/test/java/BlogTest.java
+++ src/test/java/BlogTest.java
@@ -1,40 +1,53 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.ByteArrayInputStream;
 import java.util.concurrent.TimeUnit;
 
-public class TelexTest {
+public class BlogTest {
 
-    public WebDriver driver;
-
-    @Test
-    @Epic("Telex")
-    @Story("Telex Landing Page")
-    @Description("Navigation to the landing page")
-    @Severity(SeverityLevel.CRITICAL)
-    public void TestNavigation()
-    {
+    WebDriver driver;
+    @BeforeEach
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-extensions");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("start-maximized");
+        options.addArguments("--ignore-certificate-errors");
+        //options.addArguments("user-agent="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)/%22%22);
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        driver.navigate().to("https://telex.hu");
-        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
     }
+
+
+    @Test
+    public void goToBlog() throws InterruptedException {
+
+        Main main = new Main(driver);
+        main.SignInTest();
+        main.loginTest();
+
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.goToBlog();
+        Thread.sleep(2000);
+        landingPage.seeAllPosts();
+
+        Blog blog = new Blog(driver);
+        blog.clickNext();
+
+
+
+    }
+
+
 }
